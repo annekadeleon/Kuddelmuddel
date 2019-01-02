bd73a4f7f05bd3f814a7741cd24744a80ec14a15
Anneka de Leon <annsophdl@gmail.com>
2019-01-02 23:18:24 +0800

    Update kuddelmuddel.py

--- kuddelmuddel.py
+++ kuddelmuddel.py
@@ -1,6 +1,27 @@
 print("Welcome to Kuddelmuddel!")
 print("Starting...")
 
-txt_file = open("words.txt", "r")
+words = [
+["Mann","man"],
+["Madchen" , "girl"],
+["Frau" , "woman"],
+["Junge" , "boy"],
+["Brot" , "bread"],
+["Wasser" , "water"],
+["er" , "he"],
+["sie" , "she"],
+["es" , "it"],
+["und" , "and"],
+["Kind" , "child"],
+["ich" , "I"],
+["du" , "you"],
+["bist" , "are"],
+["ist" , "is"],
+["bin" , "am"],
+]
 
-print txt_file.readlines()
+#list_name[column][row]
+#words[word][german/english]
+
+print words[0][0]
+print words[0][1]
